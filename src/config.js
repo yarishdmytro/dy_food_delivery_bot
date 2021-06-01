b397e5059fb1fd51def6b791146b4068b12c83e6
Dmytro <37596811+yarishdmytro@users.noreply.github.com>
2021-06-01 23:12:53 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  TOKEN: '1763539305:AAG_B6UVMRqcXA6Bw3wEI1DBsN7TEUgx5uM',
-  MONGODB_URL: 'mongodb+srv://admin:qwerty123@kindchallenge.b4rsh.mongodb.net/food-bot?retryWrites=true&w=majority'
+  TOKEN: '',
+  MONGODB_URL: ''
 }
